2fe59c26398050241e865a3344ae1a4078e957d3
Archil Tavdgiridze <achitavdgiridze@gmail.com>
2024-03-16 15:50:43 +0400

    Add history section to the festival page

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,7 +1,64 @@
 import React from "react";
+import year2022 from "../../assets/HistoryPage/HistoryYearPhotos/history-2022.jpg";
+import year2023 from "../../assets/HistoryPage/HistoryYearPhotos/history-2023.jpg";
+import "./History.scss";
 
 const History = () => {
-  return <div>History</div>;
+  return (
+    <div className="history_wrapper">
+      <div className="festival_history">
+        <h1>History of the festival</h1>
+        <p>
+          We founded our festival in 2022 and it was held only locally. The name
+          was simply "Adjarian News", which targeted the audience of mostly the
+          children from our region. It proved its success, we got support from
+          other regions and this year we decided to take a bold step and make it
+          international. Even though we are almost newbies and the festival has
+          existed for three years, we are ready to take the challenges and offer
+          our original vision of Georgian Smart-Film Festival to the world.
+        </p>
+      </div>
+      <div className="history_by_years">
+        <div className="year2022">
+          <div className="year_texts">
+            <h2 className="history_year">2022</h2>
+            <p>
+              THE FIRST FESTIVAL WAS HELD LOCALLY . IT WAS CALLED "ADJARIAN
+              NEWS" AND THERE WERE 19 FILMS IN THE FINALE. THE AUTHORS OF THE
+              FILM RECEIVED VARIOUS NOMINATIONS, THE FESTIVAL EXCEEDED ALL
+              EXPECTATIONS AND DREW A HUGE ATTENTION IN THE REGION. THE FESTIVAL
+              WON THE GRANT COMPETITION OF THE MINISTRY OF EDUCATION, CULTURE
+              AND SPORT OF ADJARA. SINCE THEN, THE FESTIVAL HAS THE SUPPORT OF
+              LOCAL AND NATIONAL MINISTRIES AND ORGANIZATIONS. THE MAIN
+              FINANCIAL SUPPORTER IS SHUAKHEVI CITY HALL.
+            </p>
+          </div>
+          <img src={year2022} alt="photo from the festival from 2022" />
+        </div>
+        <div className="year2023">
+          <div className="year_texts">
+            <h2 className="history_year">2023</h2>
+            <p>
+              THE SECOND FESTIVAL STEPPED UP TO AN INTERNATIONAL LEVEL . OUT OF
+              DOZENS FILMS, 19 YOUNG FILMMAKERS FROM 6 COUNTRIES MADE IT INTO
+              THE FINALE. (GEORGIA, UKRAINE, IRAN, JAPAN, FRANCE, INDIA). THE
+              AUTHORS OF THE FILM RECEIVED VARIOUS NOMINATIONS. DUE TO AN
+              EXCEPTION, BOTH OF THE GRAN-PRIX WENT TO FICTION FILMS. THE
+              FESTIVAL STARTED TO ATTRACT INTEREST FROM OTHER COUNTRIES AND
+              GAINED EVEN MORE SUPPORT NATIONALLY. THE FESTIVAL, THIS YEAR
+              AGAIN, WON THE GRANT COMPETITION OF THE MINISTRY OF EDUCATION,
+              CULTURE AND SPORT OF ADJARA.
+            </p>
+          </div>
+          <img src={year2023} alt="photo from the festival from 2023" />
+        </div>
+        <div className="year2024">
+          <h2 className="history_year">2024</h2>
+          <p>Coming soon...</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default History;
