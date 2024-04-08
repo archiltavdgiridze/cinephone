2e4c46a7e660b73ca31a8c59ce681315eb3f9b73
jeliaaa <julia.aleqsandre@gmail.com>
2024-04-08 23:43:34 +0400

    aboutUs, Submissions, Gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import './Gallery.scss'
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return <div className='gal_body'>
+    <div className="gallery">
+      <input type="checkbox" />
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((e) => (
+        <img key={e} src={'https://picsum.photos/100'} alt={e} />
+      ))}
+    </div>
+  </div>;
 };
 
 export default Gallery;
